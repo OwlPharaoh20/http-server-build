292c142bbe17f0dfc8e34b4dd39b7695d1d60bf3
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-14 05:33:42 -0700

    built and tested a few routes

--- server.js
+++ server.js
@@ -12,14 +12,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 //Define a route
-app.get( '/', (req, res) => {
-    res.send('Hello From Express1');
+app.post( '/contact', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).send('Name is required')
+    }
+
+    //DATABASE STUFF
+    res.status(201).send(`Thank You ${req.body.name}`)
 });
 
+app.post('login', (req, res) => {
+    if(!req.header('x-auth-token')) {
+        return res.status(400).send('No Token');
+    } 
+
+    if(req.header('x-auth-token') !== '123456') {
+        return res.status(401).send('Not authorized')
+
+    }
+
+    res.send('Logged in');
+})
 
 //Define Port
 const PORT = 5000;
 
 app.listen(5000, ()  => {
-    console.log(`Server started on `)
+    console.log(`Server started on Port: ${PORT} `)
 })
